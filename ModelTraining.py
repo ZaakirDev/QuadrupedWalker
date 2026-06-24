197328783f92a342adc14c403b831f011b6ae87f
BlockmasterPlayz <zaakirfazlan@outlook.com>
2026-06-24 15:43:07 +0530

    Created the ModelTraining and requirements file for people to download

---
+++ ModelTraining.py
@@ -0,0 +1,23 @@
+import pybullet
+import gymnasium as gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+
+# Create the environment
+env = gym.make("", render_mode="human") # NOTE: THIS IS SUBJECT TO CHANGE SINCE A CUSTOM ENVIRONMENT WILL BE MADE.
+model = PPO("MlpPolicy", env, verbose=1)
+TARGET_REWARD = 10000 # NOTE: THIS IS SUBJECT TO CHANGE BASED ON HOW I HAVE MADE THE ENVIRONMENT FOR THE PROJECT
+
+for i in range(10000):
+
+    model.learn(total_timesteps=10000)
+    model.save("WalkerPPO")
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
+    print(f"Mean reward: {mean_reward}")
+
+    if mean_reward >= TARGET_REWARD:
+        print("Target reward reached!")
+        break
+
+del model
