c60a2079999b3fdec42d897231d206bfb9e99f7a
BlockmasterPlayz <zaakirfazlan@outlook.com>
2026-07-03 16:08:20 +0530

    Finished __init__, _get_obs, reset and began step function development

---
+++ Environment.py
@@ -0,0 +1,123 @@
+import pybullet as p
+import pybullet_data
+import time
+import numpy as np
+import gymnasium as gym
+from torch import seed
+
+class WalkerEnv(gym.Env):
+    def __init__(self):
+        # Setup pybullet simulation
+        physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.81)
+        
+        # Declare the agent and target location
+        self._agent_location  = np.array([-1, -1, -1], dtype=np.float32) 
+        self._target_location = np.array([-1, -1, -1], dtype=np.float32)
+        
+        self.observation_space = gym.spaces.Dict(
+            {
+                "baseOrientation": gym.spaces.Box(
+                    low=np.array([-np.pi, -np.pi], dtype=np.float32),
+                    high=np.array([np.pi, np.pi], dtype=np.float32),
+                    dtype=np.float32
+                ),
+                "jointAngles": gym.spaces.Box(
+                    low=np.array([-np.pi for x in range(12)]),
+                    high=np.array([np.pi for y in range(12)]),
+                    dtype=np.float32
+                ),
+                "jointVelocities": gym.spaces.Box(
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(12,),
+                    dtype=np.float32
+                ),
+                "baseAngularVelocity": gym.spaces.Box(
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(3,),
+                    dtype=np.float32
+                ),
+                "relativeTargetPosition": gym.spaces.Box(
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(2,),
+                    dtype=np.float32
+                )
+            }
+        )
+
+        jointUpper = []
+        jointLower = []
+        for i in range(12):
+            jointInfo = p.getJointInfo(self.robot, i)
+            jointUpper.append(jointInfo[9])
+            jointLower.append(jointInfo[8])
+
+        self.action_space = gym.spaces.Box(
+            low=np.array(jointLower, dtype=np.float32),
+            high=np.array(jointUpper, dtype=np.float32),
+            dtype=np.float32
+        )
+
+    def _get_obs(self):
+        # Get and store all the observations as local variables
+        position, orientation = p.getBasePositionAndOrientation(self.robot)
+        roll, pitch, _ = p.getEularFromQuaternion(orientation) # Used for base orientation
+        joint_states = p.getJointStates(self.robot, [i for i in range(12)]) # Used for Joint Angles and joint Velocities
+        jointAngles = np.array([state[0] for state in joint_states],dtype=np.float32)
+        jointVelocities = np.array([state[1] for state in joint_states],dtype=np.float32)
+        _, baseAngularVelocity = p.getBaseVelocity(self.robot) # Used for baseAngularVelocity
+        relativeTargetPosition = np.array([self._target_location[0] - position[0], self._target_location[1] - position[1]], dtype=np.float32)
+
+        # Return all the stored observations as one dictionary with the same naming conventions and formats as the observation_space
+        return {
+            "baseOrientation": np.array([roll, pitch], dtype=np.float32),
+            "jointAngles": np.array(jointAngles, dtype=np.float32),
+            "jointVelocities": np.array(jointVelocities, dtype=np.float32),
+            "baseAngularVelocity": np.array(baseAngularVelocity, dtype=np.float32),
+            "relativeTargetPosition": relativeTargetPosition
+        }
+    
+    def reset(self, *, seed=None, options=None):
+        super().reset(seed=seed)
+        
+        agent_x = 0
+        agent_y = 0
+
+        target_x = 0
+        target_y = 0
+
+        while target_x == 0 or target_y == 0:
+            target_x = self.np_random.uniform(-5, 5)
+            target_y = self.np_random.uniform(-5, 5)
+        
+        self._agent_location = [agent_x, agent_y, 0]
+        self._target_location = [target_x, target_y, 0]
+        
+        p.resetSimulation()
+        p.setGravity(0,0,-9.81)
+        p.loadURDF("plane.urdf")
+        self.target = p.loadURDF("r2d2.urdf", self._target_location, p.getQuaternionFromEuler([0,0,0]))
+        self.robot = p.loadURDF("laikago.urdf",self._agent_location, p.getQuaternionFromEuler([0,0,0]))
+
+        observation = self._get_obs()
+        return observation
+    
+    def step(self, action):
+        # Apply the Action
+        p.setJointMotorControlArray(self.robot, [i for i in range(12)], p.POSITION_CONTROL, action)
+
+        # Advance the simulation
+        p.stepSimulation()
+
+        # Check if terminated
+
+        # Get Observations
+        observation = self._get_obs()
+
+        # Compute Reward
+
+    
