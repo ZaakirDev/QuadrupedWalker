273401e2429afd9e588108063f6f489ea2329206
BlockmasterPlayz <zaakirfazlan@outlook.com>
2026-07-03 16:06:47 +0530

    Switched to MultiInputPolicy

--- ModelTraining.py
+++ ModelTraining.py
@@ -6,7 +6,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 # Create the environment
 env = gym.make("", render_mode="human") # NOTE: THIS IS SUBJECT TO CHANGE SINCE A CUSTOM ENVIRONMENT WILL BE MADE.
-model = PPO("MlpPolicy", env, verbose=1)
+model = PPO("MultiInputPolicy", env, verbose=1) # NOTE: CHANGED FROM MLP POLICY TO MULTI INPUT POLICY SINCE A DICT IS USED FOR OBSERVATIONS
 TARGET_REWARD = 10000 # NOTE: THIS IS SUBJECT TO CHANGE BASED ON HOW I HAVE MADE THE ENVIRONMENT FOR THE PROJECT
 
 for i in range(10000):
